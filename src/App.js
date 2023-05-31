f84b905f7a1449fc170a98fdecc47d2fc8bc29b0
Frank Tadeo <86613948+EdwardTadeo@users.noreply.github.com>
2023-05-31 03:16:07 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Header from './Header';
-import Generate from './Generate'; // Asegúrate de importar tu componente Generate
-import SelectTemplate from './SelectTemplate';
-import { Button } from '@mui/material';
-import './App.css'
-import { red } from '@mui/material/colors';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Header from "./Header";
+import Generate from "./Generate"; // Asegúrate de importar tu componente Generate
+import SelectTemplate from "./SelectTemplate";
+import { Button } from "@mui/material";
+import "./App.css";
+import { red } from "@mui/material/colors";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Header />
-                <Routes>
-                    <Route path="/" element={
-                        <div className='home-container'>
-                            <h3>Generador de Curriculum en Línea</h3>
-                            <h1 className='main-title'>CREA TU CV DE MANERA GRATUITA AQUÍ</h1>
-                            <p style={{fontSize: '20px'}}>
-                                Potencia tu empleabilidad con estos diseños de CV que puede ser leido por todas las plataformas. Fácil de usar y listo en cuestión de minutos. ¡Pruébelo ahora gratis!
-                            </p>
-                            <Link to="/generate">
-                                <Button variant="contained" style={{backgroundColor: '#DF321A'}}>
-                                    Diseñar mi Curriculum
-                                </Button>
-                            </Link>
-                        </div>
-                    }/>
-                    <Route path="/generate" element={<Generate />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="home-container">
+                <h3>Generador de Curriculum en Línea</h3>
+                <h1 className="main-title">
+                  CREA TU CV DE MANERA GRATUITA AQUÍ
+                </h1>
+                <p style={{ fontSize: "20px" }}>
+                  Potencia tu empleabilidad con estos diseños de CV que puede
+                  ser leido por todas las plataformas. Fácil de usar y listo en
+                  cuestión de minutos. ¡Pruébelo ahora gratis!
+                </p>
+                <Link to="/select">
+                  <Button
+                    variant="contained"
+                    style={{ backgroundColor: "#DF321A" }}
+                  >
+                    Diseñar mi Curriculum
+                  </Button>
+                </Link>
+              </div>
+            }
+          />
+          <Route path="/select" element={<SelectTemplate />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/SelectTemplate.js
+++ src/SelectTemplate.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { Grid, Button } from '@mui/material';
-import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import CVsinexperiencia from '../src/PDF-CV/PRACTICANTE SIN EXPERIENCIA.pdf'
-import PDF_SIN from '../src/PDF-CV/cv-sin-exp.png';
-import PDF_CON from '../src/PDF-CV/cv-con-exp.png';
-import PDF_JUNIOR from '../src/PDF-CV/JUNIOR.png';
-import PDF_SENIOR from '../src/PDF-CV/Especialista.png';
-
+import React, { useState } from "react";
+import { Grid, Button, Popover, Typography } from "@mui/material";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import CVsinexperiencia from "../src/PDF-CV/PRACTICANTE SIN EXPERIENCIA.pdf";
+import PDF_SIN from "../src/PDF-CV/cv-sin-exp.png";
+import PDF_CON from "../src/PDF-CV/cv-con-exp.png";
+import PDF_JUNIOR from "../src/PDF-CV/JUNIOR.png";
+import PDF_SENIOR from "../src/PDF-CV/Especialista.png";
+import "./style.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const MyPDFs = () => {
@@ -20,46 +20,135 @@ const MyPDFs = () => {
 
   const handlePDFClick = (pdf) => {
     setSelectedPDF(pdf);
-  }
+  };
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
   return (
-    <Grid container spacing={2} style={{ height: '100vh' }}>
-      <Grid item xs={12} md={6} style={{ overflowY: 'auto', maxHeight: '100vh' }}>
+    <Grid container spacing={2} style={{ height: "100vh" }}>
+      <Grid
+        item
+        xs={12}
+        md={6}
+        style={{ overflowY: "auto", maxHeight: "100vh" }}
+      >
         <Grid container spacing={2}>
           <Grid item xs={6}>
             <Grid container direction="column" alignItems="center">
-              <img src={PDF_SIN} style={{width: '150px'}} alt="PDF 1" onClick={() => handlePDFClick(CVsinexperiencia)} />
-              <Button onClick={() => handlePDFClick(CVsinexperiencia)}>Ver PDF 1</Button>
+              <img
+                src={PDF_SIN}
+                style={{ width: "150px" }}
+                alt="PDF 1"
+                onClick={() => handlePDFClick(CVsinexperiencia)}
+              />
+              <Button onClick={() => handlePDFClick(CVsinexperiencia)}>
+                Ver PDF 1
+              </Button>
+            </Grid>
+            <div>
+              <Typography
+                aria-owns={open ? "mouse-over-popover" : undefined}
+                aria-haspopup="true"
+                onMouseEnter={handlePopoverOpen}
+                onMouseLeave={handlePopoverClose}
+                sx={{
+                  borderRadius: "50%",
+                  width: 20,
+                  height: 20,
+                  display: "flex",
+                  color: "white",
+                  background: "red",
+                  fontWeight: 800,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  border: "1px solid red",
+                  cursor: "help",
+                }}
+              >
+                ?
+              </Typography>
+              <Popover
+                id="mouse-over-popover"
+                sx={{
+                  pointerEvents: "none",
+                }}
+                open={open}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                onClose={handlePopoverClose}
+                disableRestoreFocus
+              >
+                <Typography sx={{ p: 1 }}>
+                  Este modelo de CV es ideal para personas que no cuentan con
+                  experiencia previa. Da tu primer paso con esta plantilla!!
+                </Typography>
+              </Popover>
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container direction="column" alignItems="center">
+              <img
+                src={PDF_CON}
+                style={{ width: "150px" }}
+                alt="PDF 2"
+                onClick={() => handlePDFClick(CVsinexperiencia)}
+              />
+              <Button onClick={() => handlePDFClick(CVsinexperiencia)}>
+                Ver PDF 2
+              </Button>
             </Grid>
           </Grid>
           <Grid item xs={6}>
             <Grid container direction="column" alignItems="center">
-              <img src={PDF_CON} style={{width: '150px'}} alt="PDF 2" onClick={() => handlePDFClick(CVsinexperiencia)} />
-              <Button onClick={() => handlePDFClick(CVsinexperiencia)}>Ver PDF 2</Button>
+              <img
+                src={PDF_JUNIOR}
+                style={{ width: "150px" }}
+                alt="PDF 3"
+                onClick={() => handlePDFClick(CVsinexperiencia)}
+              />
+              <Button onClick={() => handlePDFClick(CVsinexperiencia)}>
+                Ver PDF 3
+              </Button>
             </Grid>
           </Grid>
           <Grid item xs={6}>
             <Grid container direction="column" alignItems="center">
-              <img src={PDF_JUNIOR} style={{width: '150px'}} alt="PDF 3" onClick={() => handlePDFClick(CVsinexperiencia)} />
-              <Button onClick={() => handlePDFClick(CVsinexperiencia)}>Ver PDF 3</Button>
-            </Grid>
-          </Grid>
-          <Grid item xs={6}>
-            <Grid container direction="column" alignItems="center">
-              <img src={PDF_SENIOR} style={{width: '150px'}} alt="PDF 4" onClick={() => handlePDFClick(CVsinexperiencia)} />
-              <Button onClick={() => handlePDFClick(CVsinexperiencia)}>Ver PDF 4</Button>
+              <img
+                src={PDF_SENIOR}
+                style={{ width: "150px" }}
+                alt="PDF 4"
+                onClick={() => handlePDFClick(CVsinexperiencia)}
+              />
+              <Button onClick={() => handlePDFClick(CVsinexperiencia)}>
+                Ver PDF 4
+              </Button>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
-      <Grid item xs={12} md={6} style={{ overflowY: 'auto' }}>
+      <Grid item xs={12} md={6} style={{ overflowY: "auto" }}>
         {selectedPDF && (
           <Document
+            width={200}
             file={selectedPDF}
             onLoadSuccess={onDocumentLoadSuccess}
-            style={{ width: '20px', height: '20px' }}
           >
-            <Page scale={0.5} key={`page_${1}`} pageNumber={ 1} />
+            <Page pageNumber={1} width={350} />
           </Document>
         )}
       </Grid>
